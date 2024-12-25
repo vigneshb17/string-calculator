ddcf30de089bd45df06255ed81c6a0bf8d12a0fa
Vignesh Balakrishnan <vignesh@experience.com>
2024-12-25 12:50:01 +0530

    add the positive scenario test cases for add operation

--- rspec/string_calculator_rspec.rb
+++ rspec/string_calculator_rspec.rb
@@ -4,7 +4,7 @@ Rspec.describe 'StringCalculator' do
    describe '#add' do
       context 'with negative cases' do
         before :each, create_calculator_object: true do 
-           @calculator = StringCalculator.new  
+          @calculator = StringCalculator.new  
         end     
 
         it 'expects the instance value should not be matched with class instance' do
@@ -18,6 +18,40 @@ Rspec.describe 'StringCalculator' do
         it 'expects raise error as negative numbers not allowed -1, -2', :create_calculator_object do
           expect { @calculator.add("-1\n-2,3") }.to raise_error RuntimeError, 'negative numbers not allowed -1, -2'
         end
-      end   
+      end  
+      
+      context 'with positive cases' do
+        before :each do 
+          @calculator = StringCalculator.new  
+        end 
+
+        it 'expects the instance value should be matched with class instance' do
+          expect(@calculator.instance_of?(StringCalculator)).to eql true
+        end
+    
+        it 'expects the result as 0 if passed empty string as an argument' do
+          expect(@calculator.add('')).to eql 0
+        end
+    
+        it "expects the result as 3 if passed string - '1,2' as an argument" do
+          expect(@calculator.add('1,2')).to eql 3
+        end
+    
+        it "expects the result as 10 if passed '1,2,3,4' as an argument" do
+          expect(@calculator.add('1,2,3,4')).to eql 10
+        end
+    
+        it "expects the result as 6 if passed '1\n2,3' as an argument" do
+          expect(@calculator.add("1\n2,3")).to eql 6
+        end
+    
+        it "expects the result as 3 if passed '//;\n1;2' as an argument" do
+          expect(@calculator.add("//;\n1;2")).to eql 3
+        end
+    
+        it "expects the result as 12 if passed '//----\n1----2,3,6' as an argument" do
+          expect(@calculator.add("//----\n1----2,3,6")).to eql 12
+        end
+      end
    end 
-end    
+end 
