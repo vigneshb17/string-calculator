d5006ed62eacffc2bc560a5911f5997bca1e4948
Vignesh Balakrishnan <vignesh@experience.com>
2024-12-25 12:55:58 +0530

    resolve the instance issue in negative case scenario

--- rspec/string_calculator_rspec.rb
+++ rspec/string_calculator_rspec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'StringCalculator' do
         end     
 
         it 'expects the instance value should not be matched with class instance' do
-          expect(@calculator.instance_of?('StringCalculator')).to eql false  
+          expect(@calculator.instance_of?(StringCalculator)).to eql false  
         end    
 
         it 'expects raise error as negative numbers not allowed -1', :create_calculator_object do
