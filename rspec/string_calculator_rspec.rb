69d19543b7d9128a533025fab57019c8d80383d7
Vignesh Balakrishnan <vignesh@experience.com>
2024-12-25 12:30:00 +0530

    add the negative scenario test cases for add operation

--- rspec/string_calculator_rspec.rb
+++ rspec/string_calculator_rspec.rb
@@ -1,5 +1,23 @@
 require 'string_calculator'
 
 Rspec.describe 'StringCalculator' do
+   describe '#add' do
+      context 'with negative cases' do
+        before :each, create_calculator_object: true do 
+           @calculator = StringCalculator.new  
+        end     
+
+        it 'expects the instance value should not be matched with class instance' do
+          expect(@calculator.instance_of?('StringCalculator')).to eql false  
+        end    
+
+        it 'expects raise error as negative numbers not allowed -1', :create_calculator_object do
+          expect { @calculator.add("-1,2") }.to raise_error RuntimeError, 'negative numbers not allowed -1'
+        end
     
+        it 'expects raise error as negative numbers not allowed -1, -2', :create_calculator_object do
+          expect { @calculator.add("-1\n-2,3") }.to raise_error RuntimeError, 'negative numbers not allowed -1, -2'
+        end
+      end   
+   end 
 end    
